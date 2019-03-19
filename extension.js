568de07db77021d351df9d303c15f6896ae27830
Bastien Durel <bastien@durel.org>
2019-03-19 10:40:25 +0100

    Gnome 3.32 - drop pre-3.28 compatibility - switch to ES6-style classes

--- extension.js
+++ extension.js
@@ -7,6 +7,7 @@ const Shell = imports.gi.Shell;
 const St = imports.gi.St;
 const Gio = imports.gi.Gio;
 const Clutter = imports.gi.Clutter;
+const GObject = imports.gi.GObject;
 
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
@@ -14,36 +15,41 @@ const PopupMenu = imports.ui.popupMenu;
 const Panel = imports.ui.panel;
 
 const ExtensionUtils = imports.misc.extensionUtils;
+const Config = imports.misc.config;
 const Me = ExtensionUtils.getCurrentExtension();
 
 const RDSK_ICON_SIZE = 22;
 const DEFAULT_KEYBOARD = 'en-us';
 const DEFAULT_NETWORK = 'lan';
 
-const RDesktopMenuItem = new Lang.Class({
-    Name: 'RDesktopMenu.RDesktopMenuItem',
-    Extends: PopupMenu.PopupBaseMenuItem,
+let _indicator;
+window.rdesktopmenu = {
+    extdatadir: ExtensionUtils.getCurrentExtension().path,
+    shell_version: parseInt(Config.PACKAGE_VERSION.split('.')[1], 10)
+};
 
-    _init: function(conf) {
-	      this.parent();
-	      global.log('init ' + conf.name);
+var RDesktopMenuItem = class RDesktopMenuItem extends PopupMenu.PopupBaseMenuItem {
 
-	      this.label = new St.Label({ text: conf.name });
-	      this.actor.add(this.label, { expand: true });
+    constructor(conf) {
+        super();
+        global.log('init ' + conf.name);
+
+        this.label = new St.Label({ text: conf.name });
+        this.actor.add(this.label, { expand: true });
         this.actor.label_actor = this.label;
 
-	      this.conf = conf;
+        this.conf = conf;
 
         let icon_name = conf.icon_name || 'computer-symbolic';
-	      let icon = new St.Icon({ icon_name: icon_name, 
-	                               icon_size: RDSK_ICON_SIZE });
-	      let button = new St.Button({ child: icon });
-	      button.connect('clicked', Lang.bind(this, this._run));
-	      this.actor.connect('button-press-event', Lang.bind(this, this._run));
-	      this.actor.add(button);
-    },
+        let icon = new St.Icon({ icon_name: icon_name,
+                                 icon_size: RDSK_ICON_SIZE });
+        let button = new St.Button({ child: icon });
+        button.connect('clicked', Lang.bind(this, this._run));
+        this.actor.connect('button-press-event', Lang.bind(this, this._run));
+        this.actor.add(button);
+    }
 
-    _run: function() {
+    _run() {
         try {
             global.log("Try to run: '" + this.conf.run + "'");
             GLib.spawn_command_line_async(this.conf.run);
@@ -52,43 +58,40 @@ const RDesktopMenuItem = new Lang.Class({
             Main.notifyError('Error', err.message);
         }
     }
-});
+};
 
-const RDesktopRefreshMenuItem = new Lang.Class({
-    Name: 'RDesktopMenu.RDesktopMenuItem',
-    Extends: PopupMenu.PopupBaseMenuItem,
+var RDesktopRefreshMenuItem = class RDesktopRefreshMenuItem extends PopupMenu.PopupBaseMenuItem {
 
-    _init: function(conf) {
-	      this.parent();
-	      this.label = new St.Label({ text: 'Refresh' });
-	      this.actor.add(this.label, { expand: true });
+    constructor(conf) {
+        super();
+        this.label = new St.Label({ text: 'Refresh' });
+        this.actor.add(this.label, { expand: true });
         this.actor.label_actor = this.label;
 
-	      let icon = new St.Icon({ icon_name: 'view-refresh-symbolic',
-	                               icon_size: RDSK_ICON_SIZE });
-	      let button = new St.Button({ child: icon });
-	      button.connect('clicked', Lang.bind(this, this._run));
-	      this.actor.connect('button-press-event', Lang.bind(this, this._run));
-	      this.actor.add(button);
-    },
+        let icon = new St.Icon({ icon_name: 'view-refresh-symbolic',
+                                 icon_size: RDSK_ICON_SIZE });
+        let button = new St.Button({ child: icon });
+        button.connect('clicked', Lang.bind(this, this._run));
+        this.actor.connect('button-press-event', Lang.bind(this, this._run));
+        this.actor.add(button);
+    }
 
-    _run: function() {
+    _run() {
         try {
+            global.log('calling refresh()');
             _indicator.refresh()
         }
         catch (err) {
             Main.notifyError('Error', err.message);
         }
     }
-});
+};
 
 
-const RDesktopMenu = new Lang.Class({
-    Name: 'RDesktopMenu.RDesktopMenu',
-    Extends: PanelMenu.Button,
+var RDesktopMenu = class RDesktopMenu extends PanelMenu.Button {
 
-    _init: function() {
-        this.parent(0, 'server');
+    _init() {
+        super._init(0, 'server');
         this.items = [];
         let hbox = new St.BoxLayout({ style_class: 'panel-status-menu-box' });
         let icon = new St.Icon({ icon_name: 'network-workgroup-symbolic',
@@ -101,13 +104,13 @@ const RDesktopMenu = new Lang.Class({
         this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
         this._createItems();
         this.actor.show();
-    },
+    }
 
-    destroy: function() {
-        this.parent();
-    },
+    destroy() {
+        super.destroy();
+    }
 
-    _createItems: function() {
+    _createItems() {
         global.log('starting _createItems()');
         let dir = Gio.file_new_for_path(GLib.get_user_config_dir ()
                                         + "/grdesktop");
@@ -115,47 +118,44 @@ const RDesktopMenu = new Lang.Class({
         if (dir.query_exists(null)) this._listDir(dir);
 
         for (let srvid = 0; srvid < this.conf.length; srvid++) {
-            
-	        let item = new RDesktopMenuItem(this.conf[srvid]);
-	        this.menu.addMenuItem(item);
+            let item = new RDesktopMenuItem(this.conf[srvid]);
+            this.menu.addMenuItem(item);
         }
 
         this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
         this.menu.addMenuItem(new RDesktopRefreshMenuItem());
+    }
 
-    },
-
-    refresh: function() {
+    refresh() {
         this.menu.removeAll();
         this._createItems();
-    },
+    }
 
-    _getDef: function(kf, group, key, def) {
+    _getDef(kf, group, key, def) {
         return kf.has_key(group, key) ? kf.get_string(group, key) : def;
-    },
+    }
 
-
-    _getSw: function(kf, group, key, sw) {
+    _getSw(kf, group, key, sw) {
         return kf.has_key(group, key) ? ' -' + sw + ' ' +
             kf.get_string(group, key) : '';
-    },
+    }
 
-    _getXFSw: function(kf, group, key, sw) {
+    _getXFSw(kf, group, key, sw) {
         return kf.has_key(group, key) ? ' /' + sw + ':' +
             kf.get_string(group, key) : '';
-    },
+    }
     
-    _getFreeRdp: function(kf, group) {
+    _getFreeRdp(kf, group) {
         var key = 'freerdp';
         return kf.has_key(group, key) && kf.get_string(group, key) == '1';
-    },
+    }
 
-    _getExtra: function(kf, group) {
+    _getExtra(kf, group) {
         var key = 'extra';
         return kf.has_key(group, key) ? kf.get_string(group, key) : '';
-    },
+    }
 
-    _getXFRes: function(kf, group) {
+    _getXFRes(kf, group) {
         var key = 'resolution';
         if (kf.has_key(group, key)) {
             var _res = kf.get_string(group, key);
@@ -166,60 +166,60 @@ const RDesktopMenu = new Lang.Class({
                 global.log('Cannot parse resolution: ' + _res);
         }
         return ' /w:1275 /h:962';
-    },
+    }
 
-    _listDir: function(file) {
+    _listDir(file) {
         this.conf = [];
         let enumerator = file.enumerate_children(
             Gio.FILE_ATTRIBUTE_STANDARD_NAME, Gio.FileQueryInfoFlags.NONE,
             null);
-	      if (enumerator == null) throw error;
-	      let info;
+        if (enumerator == null) throw error;
+        let info;
         let re = /.*\.conf$/;
-	      while ((info = enumerator.next_file(null)) != null) {
-		        if (re.test(info.get_name())) {
+        while ((info = enumerator.next_file(null)) != null) {
+            if (re.test(info.get_name())) {
                 let kf = new GLib.KeyFile;
                 // Monkey patching: gir file describes has_key,
                 // but it's not present
-		            if (kf.has_key == undefined) kf.has_key = function(group, key) {
+                if (kf.has_key == undefined) kf.has_key = function(group, key) {
                     try {
-			                  let keys = kf.get_keys(group);
-			                  return keys[0].indexOf(key) != -1;
+                        let keys = kf.get_keys(group);
+                        return keys[0].indexOf(key) != -1;
                     } catch (e) { return false; }
-		            }
+                }
                 try {
-		                kf.load_from_file(file.get_path() + "/" + info.get_name(), 
+                    kf.load_from_file(file.get_path() + "/" + info.get_name(),
                                       GLib.KeyFileFlags.NONE);
                 } catch (e) {
                     global.log("Cannot load " + info.get_name() + ": " + e);
                     continue;
                 }
-                let name = kf.get_start_group();
-                let current = { name: name };
+                let name0 = kf.get_start_group();
+                let current = { "name": name0 };
 
-                if (kf.has_key(name, 'icon_name'))
-                    current.icon_name = kf.get_string(name, 'icon_name');
-                if (kf.has_key(name, 'run')) 
-                    current.run = kf.get_string(name, 'run');
+                if (kf.has_key(name0, 'icon_name'))
+                    current.icon_name = kf.get_string(name0, 'icon_name');
+                if (kf.has_key(name0, 'run')) 
+                    current.run = kf.get_string(name0, 'run');
                 else {
-                    let net = this._getDef(kf, name, 'network',
+                    let net = this._getDef(kf, name0, 'network',
                                            DEFAULT_NETWORK);
-                    let k = this._getSw(kf, name, 'keyboard', 'k');
-                    let res = this._getSw(kf, name, 'resolution', 'g');
-                    let host = this._getDef(kf, name, 'host', name);
-                    let t = this._getDef(kf, name, 'title', host);
-                    let user = this._getSw(kf, name, 'user', 'u');
-                    let pwd = this._getSw(kf, name, 'password', 'p');
-                    let domain = this._getSw(kf, name, 'domain', 'd');
-                    let extra = this._getExtra(kf, name);
-                    let freerdp = this._getFreeRdp(kf, name);
+                    let k = this._getSw(kf, name0, 'keyboard', 'k');
+                    let res = this._getSw(kf, name0, 'resolution', 'g');
+                    let host = this._getDef(kf, name0, 'host', name0);
+                    let t = this._getDef(kf, name0, 'title', host);
+                    let user = this._getSw(kf, name0, 'user', 'u');
+                    let pwd = this._getSw(kf, name0, 'password', 'p');
+                    let domain = this._getSw(kf, name0, 'domain', 'd');
+                    let extra = this._getExtra(kf, name0);
+                    let freerdp = this._getFreeRdp(kf, name0);
                     if (freerdp) {
                         current.run =
                             "xfreerdp /cert-ignore /sec:rdp +clipboard /bpp:24 /kbd:0x00020409 /drive:tmp,/tmp "
-                            + this._getXFRes(kf, name)
-                            + this._getXFSw(kf, name, 'user', 'u')
-                            + this._getXFSw(kf, name, 'password', 'p') 
-                            + this._getXFSw(kf, name, 'domain', 'd') + " '/t:"
+                            + this._getXFRes(kf, name0)
+                            + this._getXFSw(kf, name0, 'user', 'u')
+                            + this._getXFSw(kf, name0, 'password', 'p') 
+                            + this._getXFSw(kf, name0, 'domain', 'd') + " '/t:"
                             + t + "' /v:" + host;
                     }
                     else {
@@ -235,21 +235,20 @@ const RDesktopMenu = new Lang.Class({
                 if (kf.free != undefined) kf.free();
             }
         }
-	}
+  }
 
-});
+};
 
 function init() {
     //Convenience.initTranslations();
 }
-
-let _indicator;
 
 function enable() {
     _indicator = new RDesktopMenu();
     global.log(_indicator);
     Main.panel._rdpindicator = _indicator;
     Main.panel.addToStatusArea('rdesktop-menu', _indicator);
+    global.log("RDesktopMenu enabled");
 }
 
 function disable() {
@@ -257,3 +256,13 @@ function disable() {
     Main.panel._rdpindicator = null;
 }
 
+/**
+ * Re-wrap the Indicator class as a GObject subclass for GNOME Shell 3.32
+ */
+if (rdesktopmenu.shell_version >= 32) {
+    global.log("Re-wrap RDesktopMenu");
+    RDesktopMenu = GObject.registerClass(
+        {GTypeName: 'RDesktopMenuIndicator'},
+        RDesktopMenu
+    );
+}
